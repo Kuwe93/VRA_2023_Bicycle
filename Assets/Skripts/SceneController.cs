9ae6fc3874341c4200b0face86d0f04aeaf95b42
Kuwe93 <38717376+Kuwe93@users.noreply.github.com>
2024-02-04 16:25:22 +0100

    Trainingsoption implemented

--- Assets/Skripts/SceneController.cs
+++ Assets/Skripts/SceneController.cs
@@ -7,12 +7,31 @@ using System;
 
 public enum Difficulty { NoHelp = 0, SmallHelp = 1, BigHelp = 2 };
 
+public struct SavedBicyclePosition
+{
+    public int bicyclePartArrayPos;
+    public Transform trainingPosition;
+
+    //Constructor
+    public SavedBicyclePosition(int pos, Transform tPosition)
+    {
+        this.bicyclePartArrayPos = pos;
+        this.trainingPosition = tPosition;
+    }
+}
+
 public class SceneController : MonoBehaviour
 {
     private List<Difficulty> sceneDifficulty;
     public PressableButton noHelpToggle;
 
     public PressableButton[] missionToggles;
+
+    public List<GameObject> bicycleParts;
+
+    public Transform[] bicycleTransforms;
+    private Transform[] startPositions;
+    private List<SavedBicyclePosition> savedPositions;
 
     private bool timerActive;
     private float currentTimeInTimer;
@@ -21,6 +40,8 @@ public class SceneController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        savedPositions = new List<SavedBicyclePosition>();
+        startPositions = bicycleTransforms;
         currentTimeInTimer = 0;
         sceneDifficulty = new List<Difficulty>();
         this.AddDifficulty(0);
@@ -43,6 +64,10 @@ public class SceneController : MonoBehaviour
             this.AddDifficulty(0);
             noHelpToggle.ForceSetToggled(true, false);
         }
+
+        //test if saved transforms change after moving the actual objectw
+        Debug.Log("Transform 0 is:" + savedPositions[0].trainingPosition.position.x + "," + savedPositions[0].trainingPosition.position.y + "," + savedPositions[0].trainingPosition.position.z);
+
         /*foreach (var diff in sceneDifficulty)
         {
             Debug.Log("Actual Difficulty: " + diff);
@@ -132,12 +157,14 @@ public class SceneController : MonoBehaviour
 
     public void StartTraining()
     {
-        
         //starte Timer
         StartTimer();
+        //clear savedPositions
+        savedPositions.Clear();
         //ordne die Fahrradteile Zufällig neu an 
+        RandomizeObjectPositions();
         //setze die Aufgaben Toggles zurück
-        
+        ResetToggles();
     }
 
     public void resetTraining()
@@ -146,7 +173,9 @@ public class SceneController : MonoBehaviour
         StopTimer();
         ResetTimer();
         //Aufgaben Toggles zurück setzen
+        ResetToggles();
         //Fahrradteile auf startposition zurücksetzen
+        ResetObjectPositions();
     }
 
     public void StartTimer()
@@ -167,5 +196,59 @@ public class SceneController : MonoBehaviour
     public void CloseApp()
     {
         Application.Quit();
+    }
+
+    private void ResetObjectPositions()
+    {
+        foreach (var bicyclepart in savedPositions)
+        {
+            bicycleParts[bicyclepart.bicyclePartArrayPos].transform.position = bicyclepart.trainingPosition.position;
+            bicycleParts[bicyclepart.bicyclePartArrayPos].transform.rotation = bicyclepart.trainingPosition.rotation;
+            bicycleParts[bicyclepart.bicyclePartArrayPos].transform.localScale = bicyclepart.trainingPosition.localScale;
+        }
+    }
+
+    public void RandomizeObjectPositions()
+    {
+        List<Transform> tempTrans = new List<Transform>();
+        foreach(var pos in startPositions)
+        {
+            tempTrans.Add(pos);
+        }
+        // for each bicyclepart get a random position from the startposition array
+        for(int x = 0;x < bicycleParts.Count; x++)
+        {
+            int tempnr = UnityEngine.Random.Range(0, tempTrans.Count);
+            SavedBicyclePosition trainingsItem = new SavedBicyclePosition(x, tempTrans[tempnr]);
+            tempTrans.Remove(tempTrans[tempnr]);
+            savedPositions.Add(trainingsItem);
+        }
+
+        PositionObjects();
+    }
+
+    private void PositionObjects()
+    {
+        for (int x = 0; x < bicycleParts.Count; x++)
+        {
+            foreach(var pos in savedPositions)
+            {
+                if(pos.bicyclePartArrayPos == x)
+                {
+                    bicycleParts[x].transform.position = pos.trainingPosition.position;
+                    bicycleParts[x].transform.rotation = pos.trainingPosition.rotation;
+                    bicycleParts[x].transform.localScale = pos.trainingPosition.localScale;
+                }
+            }
+            
+        }
+    }
+
+    private void ResetToggles()
+    {
+        foreach(var toggle in missionToggles)
+        {
+            toggle.ForceSetToggled(false, false);
+        }
     }
 }
